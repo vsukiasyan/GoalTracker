7ac84603cd359a04a436142eddf4ec957a2784fc
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-28 23:27:28 -0700

    Added assets and initial view.

--- GoalTracker/Controller/GoalsVC.swift
+++ GoalTracker/Controller/GoalsVC.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GoalsVC: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addGoalBtnWasPressed(_ sender: UIButton) {
     }
-
+    
 
 }
 
---
+++ GoalTracker/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  GoalTracker
+//
+//  Created by Vic Sukiasyan on 4/28/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    func configureCell(description: String, type: String, gaolProgressAmount: Int) {
+        self.goalDescLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: gaolProgressAmount)
+    }
+}
