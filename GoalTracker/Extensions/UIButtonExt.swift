ec822dfded77dc7ea44803dd3e9adbd4fb3f2556
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-30 14:07:47 -0700

    Fixed another bug where the Create Goal button in the FinishGoalVC slides down as soon as you start typing.

--- GoalTracker/Extensions/UIButtonExt.swift
+++ GoalTracker/Extensions/UIButtonExt.swift
@@ -14,6 +14,6 @@ extension UIButton {
     }
     
     func setDeselectedColor() {
-        self.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        self.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 0.4468375428)
     }
 }
